afb5240f6d3d363e0a9bffe865e4ac1851f4abc9
ben <benmclaughlin179@gmail.com>
2021-06-16 13:49:26 +0300

    test

---
+++ src/firstexercise.py
@@ -0,0 +1,6 @@
+import math
+
+def calculate_time(height):
+    g = 9.81
+    result = math.sqrt (2*(height)/g)
+    return result
---
+++ tests/test_firstexercise.py
@@ -0,0 +1,3 @@
+from src.firstexercise import calculate_time
+def test_calculate_time():
+    assert calculate_time(4.905)==1
